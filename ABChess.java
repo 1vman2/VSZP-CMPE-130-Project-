5df4ffaebcf57b5e399e3761f2801a41bc3cc3a9
ZackeryPlovanic <zackeryplovanic@gmail.com>
2017-11-26 18:20:26 -0800

    Bug fixed, King replacing adjacent pieces

--- ABChess.java
+++ ABChess.java
@@ -94,34 +94,34 @@ public class ABChess {
 
 	public static void makeMove(String move) {
 		if (move.charAt(4)!='P') {
-			chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]
-					=chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))];
-			chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]=" ";
-		if("A".equals(chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))])){
-			kingPositionC = 8*Character.getNumericValue(move.charAt(0))+Character.getNumericValue(move.charAt(1));
-		}
-		} else {
-			//if pawn promotion
-			chessBoard[1][Character.getNumericValue(move.charAt(0))]=" ";
-			chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(3));
-		}
-	}
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]
+            		=chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))];
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]=" ";
+            if ("A".equals(chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))])) {
+                kingPositionC=8*Character.getNumericValue(move.charAt(2))+Character.getNumericValue(move.charAt(3));
+            }
+        } else {
+            //if pawn promotion
+            chessBoard[1][Character.getNumericValue(move.charAt(0))]=" ";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(3));
+        }
+    }
 	
 	public static void undoMove(String move){
 		if (move.charAt(4)!='P') {
-			chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]
-					=chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))];
-			chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]
-					=String.valueOf(move.charAt(4));
-			if("A".equals(chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))])){
-				kingPositionC = 8*Character.getNumericValue(move.charAt(2))+Character.getNumericValue(move.charAt(3));
-			}
-		} else {
-			//if pawn promotion
-			chessBoard[1][Character.getNumericValue(move.charAt(0))]="P";
-			chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(2));
-		}
-	}
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]
+            		=chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))];
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]
+            		=String.valueOf(move.charAt(4));
+            if ("A".equals(chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))])) {
+                kingPositionC=8*Character.getNumericValue(move.charAt(0))+Character.getNumericValue(move.charAt(1));
+            }
+        } else {
+            //if pawn promotion
+            chessBoard[1][Character.getNumericValue(move.charAt(0))]="P";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(2));
+        }
+    }
 	
 	public static String posibleMoves(){
 		String list="";
@@ -129,18 +129,17 @@ public class ABChess {
 			switch (chessBoard[i/8][i%8]){
 
 			case("P"):list+=possibleP(i);
-			break;
+				break;
 			case("R"):list+=possibleR(i);
-			break;
+				break;
 			case("K"):list+=possibleK(i);
-			break;
+				break;
 			case("B"):list+=possibleB(i);
-			break;
+				break;
 			case("Q"):list+=possibleQ(i);
-			break;
+				break;
 			case("A"):list+=possibleA(i);
-			break;
-
+				break;
 
 			}
 		}
